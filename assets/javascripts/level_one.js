d6f57012bea8bf6ce9a9a0a4a407f275ccf614e6
Jack Slinger <j.slinger@sheffield.ac.uk>
2016-08-27 17:19:56 +0100

    Added a simple lock and key puzzle with blue and red keys.

--- assets/javascripts/level_one.js
+++ assets/javascripts/level_one.js
@@ -1,3 +1,8 @@
+var keys = {
+  blueKey: 0,
+  redKey: 0
+}
+
 function winGame() {
   game.state.start('win');
 }
@@ -6,9 +11,32 @@ function loseGame() {
   game.state.start('lose');
 }
 
-var keys = {
-  blueKey: false,
-  redKey: false
+function collectBlueKey(player, key) {
+  keys.blueKey += 1;
+  blueKeyText.text = keys.blueKey;
+  key.kill();
+}
+
+function collectRedKey(player, key) {
+  keys.redKey += 1;
+  redKeyText.text = keys.redKey;
+  key.kill();
+}
+
+function collideWithRedLock(player, lock) {
+  if (keys.redKey > 0) {
+    keys.redKey -= 1;
+    redKeyText.text = keys.redKey;
+    lock.kill();
+  }
+}
+
+function collideWithBlueLock(player, lock) {
+  if (keys.blueKey > 0) {
+    keys.blueKey -= 1;
+    blueKeyText.text = keys.blueKey;
+    lock.kill();
+  }
 }
 
 levelOneState = {
@@ -16,24 +44,43 @@ levelOneState = {
     this.load.tilemap('map', 'assets/images/test_map.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('tiles', 'assets/images/test_tiles.png');
     this.load.image('player', 'assets/images/player_block.png');
-    this.load.image('golden_skull', 'assets/images/golden_skull.png');
-    this.load.image('spikes', 'assets/images/spikes.png');
-    this.load.image('small_spikes', 'assets/images/small_spikes.png');
+    this.load.image('goldenSkull', 'assets/images/golden_skull.png');
+    this.load.image('spikes', 'assets/images/spikes_new.png');
+    this.load.image('keys', 'assets/images/keys.png');
+    this.load.image('blueKey', 'assets/images/blue_key.png');
+    this.load.image('redKey', 'assets/images/red_key.png');
+    this.load.image('redKeyHole', 'assets/images/red_key_hole.png');
+    this.load.image('blueKeyHole', 'assets/images/blue_key_hole.png');
+
+    this.load.bitmapFont('nokia', 'assets/fonts/nokia.png', 'assets/fonts/nokia.xml');
   },
   create: function () {
+    this.stage.backgroundColor = "#4488AA";
+
     this.physics.startSystem(Phaser.Physics.ARCADE);
     // this.physics.arcade.gravity.y = 300;
 
     map = this.add.tilemap('map');
     map.addTilesetImage('test_tiles', 'tiles');
-    // map.addTilesetImage('small_spikes', 'small_spikes');
 
-    main_layer = map.createLayer(0);
-    // spike_layer = map.createLayer('Spike Layer')
+    layer = map.createLayer(0);
     map.setCollisionBetween(2, 2);
-    // map.setCollisionBetween(1, 3, spike_layer);
 
-    main_layer.resizeWorld();
+    layer.resizeWorld();
+
+    inventoryGroup = this.add.group();
+
+    inventoryBlueKey = this.add.sprite(0, 0, 'blueKey');
+    inventoryBlueKey.fixedToCamera = true;
+
+    blueKeyText = this.add.bitmapText(40, 7, 'nokia', '0', 16);
+    blueKeyText.fixedToCamera = true;
+
+    inventoryRedKey = this.add.sprite(0, 32, 'redKey');
+    inventoryRedKey.fixedToCamera = true;
+
+    redKeyText = this.add.bitmapText(40, 39, 'nokia', '0', 16);
+    redKeyText.fixedToCamera = true;
 
     player = this.add.sprite(0, 1536, 'player')
     this.physics.arcade.enable(player);
@@ -45,11 +92,35 @@ levelOneState = {
 
     skulls = game.add.group();
     skulls.enableBody = true;
-    map.createFromObjects('Skull Layer', 4, 'golden_skull', 0, true, false, skulls)
+    map.createFromObjects('Skull Layer', 4, 'goldenSkull', 0, true, false, skulls)
 
     spikes = game.add.group();
     spikes.enableBody = true;
     map.createFromObjects('Spike Layer', 3, 'spikes', 0, true, false, spikes);
+
+    blueKeys = game.add.group();
+    blueKeys.enableBody = true;
+    map.createFromObjects('Key Layer', 5, 'blueKey', 0, true, false, blueKeys);
+
+    blueKeyHoles = game.add.group();
+    blueKeyHoles.enableBody = true;
+    map.createFromObjects('Key Layer', 6, 'blueKeyHole', 0, true, false, blueKeyHoles);
+
+    for (i in blueKeyHoles.hash) {
+      blueKeyHoles.hash[i].body.immovable = true;
+    }
+
+    redKeys = game.add.group();
+    redKeys.enableBody = true;
+    map.createFromObjects('Key Layer', 7, 'redKey', 0, true, false, redKeys);
+
+    redKeyHoles = game.add.group();
+    redKeyHoles.enableBody = true;
+    map.createFromObjects('Key Layer', 8, 'redKeyHole', 0, true, false, redKeyHoles);
+
+    for (i in redKeyHoles.hash) {
+      redKeyHoles.hash[i].body.immovable = true;
+    }
 
     for (i in spikes.hash) {
       spikes.hash[i].body.setSize(32, 12, 0, 20);
@@ -58,9 +129,13 @@ levelOneState = {
     keyboard = this.input.keyboard.createCursorKeys();
   },
   update: function () {
-    this.physics.arcade.collide(player, main_layer);
+    this.physics.arcade.collide(player, layer);
     this.physics.arcade.collide(player, skulls, winGame);
     this.physics.arcade.collide(player, spikes, loseGame);
+    this.physics.arcade.collide(player, blueKeys, collectBlueKey);
+    this.physics.arcade.collide(player, redKeys, collectRedKey);
+    this.physics.arcade.collide(player, redKeyHoles, collideWithRedLock);
+    this.physics.arcade.collide(player, blueKeyHoles, collideWithBlueLock);
 
     player.body.velocity.x = 0
 
