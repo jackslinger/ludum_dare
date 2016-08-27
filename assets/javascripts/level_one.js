669e25b085013eecae706d4af45b852967c7d27c
Jack Slinger <j.slinger@sheffield.ac.uk>
2016-08-27 16:10:08 +0100

    Allow spikes and skulls to be set in tiled.

--- assets/javascripts/level_one.js
+++ assets/javascripts/level_one.js
@@ -6,6 +6,11 @@ function loseGame() {
   game.state.start('lose');
 }
 
+var keys = {
+  blueKey: false,
+  redKey: false
+}
+
 levelOneState = {
   preload: function() {
     this.load.tilemap('map', 'assets/images/test_map.json', null, Phaser.Tilemap.TILED_JSON);
@@ -13,6 +18,7 @@ levelOneState = {
     this.load.image('player', 'assets/images/player_block.png');
     this.load.image('golden_skull', 'assets/images/golden_skull.png');
     this.load.image('spikes', 'assets/images/spikes.png');
+    this.load.image('small_spikes', 'assets/images/small_spikes.png');
   },
   create: function () {
     this.physics.startSystem(Phaser.Physics.ARCADE);
@@ -20,10 +26,14 @@ levelOneState = {
 
     map = this.add.tilemap('map');
     map.addTilesetImage('test_tiles', 'tiles');
-    map.setCollisionBetween(2,2);
+    // map.addTilesetImage('small_spikes', 'small_spikes');
 
-    layer = map.createLayer(0);
-    layer.resizeWorld();
+    main_layer = map.createLayer(0);
+    // spike_layer = map.createLayer('Spike Layer')
+    map.setCollisionBetween(2, 2);
+    // map.setCollisionBetween(1, 3, spike_layer);
+
+    main_layer.resizeWorld();
 
     player = this.add.sprite(0, 1536, 'player')
     this.physics.arcade.enable(player);
@@ -33,20 +43,13 @@ levelOneState = {
     player.body.gravity.y = 300;
     this.camera.follow(player);
 
-    skull = this.add.sprite((22 * 32), (36 * 32), 'golden_skull');
-    this.physics.arcade.enable(skull);
+    skulls = game.add.group();
+    skulls.enableBody = true;
+    map.createFromObjects('Skull Layer', 4, 'golden_skull', 0, true, false, skulls)
 
-    spikes = game.add.group(undefined, undefined, undefined, true);
-    spikes.create((4 * 32), (48 * 32), 'spikes');
-    spikes.create((5 * 32), (48 * 32), 'spikes');
-    spikes.create((6 * 32), (48 * 32), 'spikes');
-
-    spikes.create((8 * 32), (46 * 32), 'spikes');
-    spikes.create((10 * 32), (44 * 32), 'spikes');
-    spikes.create((12 * 32), (42 * 32), 'spikes');
-    spikes.create((14 * 32), (40 * 32), 'spikes');
-
-
+    spikes = game.add.group();
+    spikes.enableBody = true;
+    map.createFromObjects('Spike Layer', 3, 'spikes', 0, true, false, spikes);
 
     for (i in spikes.hash) {
       spikes.hash[i].body.setSize(32, 12, 0, 20);
@@ -55,8 +58,8 @@ levelOneState = {
     keyboard = this.input.keyboard.createCursorKeys();
   },
   update: function () {
-    this.physics.arcade.collide(player, layer);
-    this.physics.arcade.collide(player, skull, winGame);
+    this.physics.arcade.collide(player, main_layer);
+    this.physics.arcade.collide(player, skulls, winGame);
     this.physics.arcade.collide(player, spikes, loseGame);
 
     player.body.velocity.x = 0
